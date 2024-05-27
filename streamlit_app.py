10771ed2bd71b8079d4e1ecd8f86ddaddc2ec133
Leonardo Bruno Pereira de Araújo <leonardobruno.araujo@gmail.com>
2024-05-27 08:44:07 -0300

    Update streamlit_app.py
    Add call Fruityvice API requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 
 from snowflake.snowpark.functions import col
 
@@ -47,3 +47,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
