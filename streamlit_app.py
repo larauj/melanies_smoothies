63fe558b024818f8b7e845a0bb9c2276a24e8366
Leonardo Bruno Pereira de Araújo <leonardobruno.araujo@gmail.com>
2024-05-27 08:53:10 -0300

    Update streamlit_app.py
    Reposicionar o dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
+        
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -47,7 +49,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
